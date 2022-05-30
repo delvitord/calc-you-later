0043f57dfa6e60f9dbf506d6c829e74cd3c8c5c6
delvitord <91855033+delvitord@users.noreply.github.com>
2022-05-30 22:21:35 +0700

    Add convert from / to Biner

--- test/calculatorTest.c
+++ test/calculatorTest.c
@@ -2,6 +2,8 @@
 #include "calculatorTree.h"
 #include "calculatorStack.h"
 #include "calculatorMain.h"
+#include "stackChar.h"
+#include "stackInt.h"
 
 char read_operator; // variable for reading a charactor
 Root *root;
@@ -11,7 +13,40 @@ address P;
 void InputError();
 void CheckAndGetChar(char math_opr);
 
+void chooseMenu(){ 
+	int pilihan, desimal;
+	do {
+		printf("==== Calc-You-Later ===="); 
+		printf("\n1. Calculator"); 
+		printf("\n2. Biner to Decimal"); 
+		printf("\n3. Decimal to Biner"); 
+		printf("\n4. Quit"); 
+		printf("\nMasukkan Pilihan anda : "); 
+		scanf("%d", &pilihan);
+		printf("\n");
+		switch (pilihan){
+			case 1:
+				mainCalculator();
+				break;
+			case 2:
+				binToDec();
+				break;
+			case 3:
+				printf("\nMasukkan desimal yang akan dikonversi : "); 
+				scanf("%d", &desimal);
+				decToBin(desimal);
+				break;
+		}
+	} while(pilihan < 4);
+
+} 
+
 int main(){
+	/* Menampilkan Menu pada saat program dijalankan */ 
+	chooseMenu(); 
+}
+
+void mainCalculator(){
 	double value; //akan diisi oleh nilai akhir dari operasi hitung
 	StackNode Snode;
 	TreeNode *Tnode = NULL;
@@ -22,6 +57,7 @@ int main(){
 	stack = createStack(); //buat stack kosong
 
 	/// process
+	char dump = getchar();
     read_operator = getchar(); //read operator agar operasi bilangan dapat dilakukan pada sum(), term(), dan factor() sehingga proses perhitungan dapat dilakukan
     value = sum(); // value diisi sum() karena sum() merupakan proses terakhir dalam operasi matematika sehingga keluaran dari sum() merupakan hasil akhir (value)
     
@@ -56,11 +92,73 @@ int main(){
     printf("postfix : ");
     traversal(root,POSTORDER);
     puts("");
-    printf("Hasil Operasi : %g\n", value);
+    printf("Hasil Operasi : %g\n\n", value);
+    
+    printf("Ingin merubahnya ke dalam bentuk biner? (Y untuk lanjutkan): ", value);
+    dump = getchar();
+    char isToBiner = getchar();
+    if (isToBiner = 'Y'){
+    	int toBiner = value;
+    	decToBin(toBiner);
+	}
+    
+    
+    
 	
 	/// delete
     deleteAll(P);
     return 0;
+}
+
+void binToDec(){
+	stackChar s; 
+	createstackChar(&s); 
+	int pilihan, hasil_desimal = 0, nilai_pop2;
+	char nilai_stack, nilai_pop1; 
+	printf("Masukkan bilangan biner: ");
+	char dump; 
+	scanf("%c", &dump);
+    do {
+    	scanf("%c", &nilai_stack);
+    	if (nilai_stack != 10){
+    		pushChar(&s, nilai_stack);
+		}
+	} while (nilai_stack != 10);	//10 adalah ASCII untuk enter
+	
+	stackInt biner; 
+	createstackInt(&biner); 
+	int i = s.jumlahTumpukan;
+	int value;
+	
+	for (i; i >= 0; i--){
+		value = pow(2,i);
+		pushInt(&biner, value);
+		
+	}
+
+	while (s.jumlahTumpukan > 0){
+		popChar(&s, &nilai_pop1);
+		popInt(&biner, &nilai_pop2);
+		if (nilai_pop1 == '1'){
+			hasil_desimal += nilai_pop2;
+		}
+	}
+	printf("\nHasil desimalnya adalah:");
+	printf("%d\n\n", hasil_desimal);
+}
+
+void decToBin(int desimal){
+	int hasil_modulus;
+	stackInt toBin; 
+	createstackInt(&toBin); 
+	while(desimal > 0){
+		hasil_modulus = desimal % 2;
+		pushInt(&toBin, hasil_modulus);
+		desimal /= 2;
+	}
+	printf("\nBilangan binernya adalah: \n");
+	cetakstackBiner(toBin);
+	printf("\n\n");
 }
 
 // urutan program : faktor (cek kondisi) >> square (pangkat) >> term (kali dan bagi) >> sum (tambah dan kurang)
